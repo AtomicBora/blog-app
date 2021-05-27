16ab318ab95c34256a95d59df37749bdfbf45753
TurboBora <devborivoje@gmail.com>
2021-05-27 20:16:31 +0200

    dodata komponenta AppPosts, postavljena ruta za istu i definisan router link za tu komponentu

---
+++ src/components/AppPosts.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            posts: [{
+                
+            }]
+        }
+    },
+    created() {
+        console.log("Cao")
+    },
+}
+</script>
+<style lang="">
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import AppPosts from '../components/AppPosts.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    redirect: "/posts"
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: "/posts",
+    name: "AppPosts",
+    component: AppPosts
+  }
 ];
 
 const router = new VueRouter({
